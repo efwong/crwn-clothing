6a25b26e520e1d380fb299499b5d38ff1619ebdc
Edwin <edwinfwong@gmail.com>
2020-01-17 02:16:05 -0800

    Updated navigation. Refactored shop.data.js

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -6,14 +6,21 @@ import storage from 'redux-persist/lib/storage';
 
 import userReducer from './user/user.reducer';
 import cartReducer from './cart/cart.reducer';
+import directoryReducer from './directory/directory.reducer';
+import shopReducer from './shop/shop.reducer';
 
 const persistConfig = {
   key: 'root',
   storage,
-  whitelist: ['cart'] // array of names of reducer that we want to store
+  whitelist: ['cart'] // array of names of reducer that we want to store & persist betweens essions
 };
 
-const rootReducer = combineReducers({ user: userReducer, cart: cartReducer });
+const rootReducer = combineReducers({
+  user: userReducer,
+  cart: cartReducer,
+  directory: directoryReducer,
+  shop: shopReducer
+});
 
 // modified rootReducer with persit ability from persistConfig
 export default persistReducer(persistConfig, rootReducer);

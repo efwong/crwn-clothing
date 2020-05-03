9585e669a45968221dc7de8667e18e7ab407ac36
Edwin <edwinfwong@gmail.com>
2020-05-03 14:35:42 -0700

    Add react routing to components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
+import { Route, Switch } from 'react-router-dom';
 
-function App() {
+const HatsPage = () => {
   return (
     <div>
-      <HomePage />
+      <h1>HATS PAGE</h1>
+    </div>
+  );
+};
+
+
+function App() {
+  // use switch: as long as one route is rendered, it won't render the rest
+  return (
+    <div>
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/hats' component={HatsPage} />
+      </Switch>
     </div>
   );
 }

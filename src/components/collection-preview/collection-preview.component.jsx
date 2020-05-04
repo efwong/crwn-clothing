c04a88e8d90135cb3aedc990cdfbd1c4c844b82b
Edwin <edwinfwong@gmail.com>
2020-05-04 00:08:57 -0700

    Add collection-item component and replace static code in collection-preview

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -1,16 +1,17 @@
-import React from "react";
+import React from 'react';
 
-import "./collection-preview.styles.scss";
+import './collection-preview.styles.scss';
+import CollectionItem from '../collection-item/collection-item.component';
 
 const CollectionPreview = ({ title, items }) => {
   return (
-    <div className="collection-preview">
+    <div className='collection-preview'>
       <h1>{title.toUpperCase()}</h1>
-      <div className="preview">
+      <div className='preview'>
         {items
           .filter((item, idx) => idx < 4)
-          .map((item) => (
-            <div key={item.id}>{item.name}</div>
+          .map(({ id, ...itemProps }) => (
+            <CollectionItem key={id} {...itemProps} />
           ))}
       </div>
     </div>

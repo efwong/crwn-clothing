ba4497ff347b80bb8c62868f8211b5564c9cd8e9
Edwin <edwinfwong@gmail.com>
2020-05-05 00:29:02 -0700

    Added header and link to shop

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
-import React from "react";
-import "./App.css";
-import HomePage from "./pages/homepage/homepage.component";
-import { Route, Switch } from "react-router-dom";
-import ShopPage from "./pages/shop/shop.component";
+import React from 'react';
+import './App.css';
+import HomePage from './pages/homepage/homepage.component';
+import { Route, Switch } from 'react-router-dom';
+import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
 
 function App() {
   // use switch: as long as one route is rendered, it won't render the rest
   return (
     <div>
+      <Header />
       <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route path="/shop" component={ShopPage} />
+        <Route exact path='/' component={HomePage} />
+        <Route path='/shop' component={ShopPage} />
       </Switch>
     </div>
   );

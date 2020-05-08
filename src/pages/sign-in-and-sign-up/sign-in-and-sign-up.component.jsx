7992cd995625e372cb07876671210da9ef026500
Edwin <edwinfwong@gmail.com>
2020-05-08 01:40:14 -0700

    Create Sign In and Sign Up Components. Implement form-input and custom-button components

---
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import './sign-in-and-sign-up.styles.scss';
+import SignIn from './../../components/sign-in/sign-in.component';
+
+const SignInandSignUpPage = () => (
+  <div className='sign-in-and-sign-up'>
+    <SignIn />
+  </div>
+);
+
+export default SignInandSignUpPage;

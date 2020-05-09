f88efeb4d554869db857276c4f9332f6a0fa4615
Edwin <edwinfwong@gmail.com>
2020-05-08 23:53:26 -0700

    Update firebase config

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,19 +1,9 @@
 import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
+import { FirebaseConfig } from './firebase.config';
 
-const config = {
-  apiKey: 'AIzaSyC3VccY1O7Wpu-yRJdKxWjuCNJkQFbbYP8',
-  authDomain: 'crwn-service.firebaseapp.com',
-  databaseURL: 'https://crwn-service.firebaseio.com',
-  projectId: 'crwn-service',
-  storageBucket: 'crwn-service.appspot.com',
-  messagingSenderId: '686037421637',
-  appId: '1:686037421637:web:3f3b2e56b74df3caf29d2c',
-  measurementId: 'G-F22NJEWGJ5',
-};
-
-firebase.initializeApp(config);
+firebase.initializeApp(FirebaseConfig);
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();

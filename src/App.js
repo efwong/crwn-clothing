648fb902d5d1630305614a6b78a101355eff1ff9
Edwin <edwinfwong@gmail.com>
2020-05-09 22:21:33 -0700

    Added google styling to custom button. Added signout functionality

--- src/App.js
+++ src/App.js
@@ -5,19 +5,43 @@ import { Route, Switch } from 'react-router-dom';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
 import SignInandSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+import { auth } from './firebase/firebase.utils';
 
-function App() {
-  // use switch: as long as one route is rendered, it won't render the rest
-  return (
-    <div>
-      <Header />
-      <Switch>
-        <Route exact path='/' component={HomePage} />
-        <Route path='/shop' component={ShopPage} />
-        <Route path='/signin' component={SignInandSignUpPage} />
-      </Switch>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      currentUser: null,
+    };
+  }
+
+  unsubscribeFromAuth = null;
+
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged((user) => {
+      this.setState({ currentUser: user });
+      console.log('user', user);
+    });
+  }
+
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
+  }
+
+  render() {
+    const { currentUser } = this.state;
+    // use switch: as long as one route is rendered, it won't render the rest
+    return (
+      <div>
+        <Header currentUser={currentUser} />
+        <Switch>
+          <Route exact path='/' component={HomePage} />
+          <Route path='/shop' component={ShopPage} />
+          <Route path='/signin' component={SignInandSignUpPage} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;

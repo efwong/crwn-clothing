381d5ff9295c96ab56d75c4bc01a4b09a6e1efb9
Edwin <edwinfwong@gmail.com>
2020-05-17 00:41:11 -0700

    Updated app state. Added cart icon

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -6,6 +6,7 @@ import { Link } from 'react-router-dom';
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 import { auth } from '../../firebase/firebase.utils';
 import { connect } from 'react-redux';
+import CartIcon from './../cart-icon/cart-icon.component';
 
 const Header = ({ currentUser }) => (
   <div className='header'>
@@ -29,6 +30,7 @@ const Header = ({ currentUser }) => (
           SIGN IN
         </Link>
       )}
+      <CartIcon />
     </div>
   </div>
 );

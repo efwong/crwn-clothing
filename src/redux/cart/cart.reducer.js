8cbca6cee3699f36344e20acf40a81938908de99
Edwin <edwinfwong@gmail.com>
2020-05-17 13:44:25 -0700

    Added cart utils to handle adding cart items

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,4 +1,6 @@
 import CartActionTypes from './cart.types';
+import { addItemToCart } from './cart.utils';
+
 const INITIAL_STATE = {
   hidden: true,
   cartItems: []
@@ -9,7 +11,7 @@ const cartReducer = (state = INITIAL_STATE, { type, payload }) => {
     case CartActionTypes.TOGGLE_CART_HIDDEN:
       return { ...state, hidden: !state.hidden };
     case CartActionTypes.ADD_ITEM:
-      return { ...state, cartItems: [...state.cartItems, payload] };
+      return { ...state, cartItems: addItemToCart(state.cartItems, payload) };
 
     default:
       return state;

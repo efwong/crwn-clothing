93d56376519264f20ed0e7f3cd16d4d97468c830
Edwin <edwinfwong@gmail.com>
2020-05-18 01:33:14 -0700

    Implement x Remove feature and arrows functionality to add and remove items

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,5 +1,5 @@
 import CartActionTypes from './cart.types';
-import { addItemToCart } from './cart.utils';
+import { addItemToCart, removeItemFromCart } from './cart.utils';
 
 const INITIAL_STATE = {
   hidden: true,
@@ -10,8 +10,23 @@ const cartReducer = (state = INITIAL_STATE, { type, payload }) => {
   switch (type) {
     case CartActionTypes.TOGGLE_CART_HIDDEN:
       return { ...state, hidden: !state.hidden };
+
     case CartActionTypes.ADD_ITEM:
       return { ...state, cartItems: addItemToCart(state.cartItems, payload) };
+
+    case CartActionTypes.REMOVE_ITEM:
+      return {
+        ...state,
+        cartItems: removeItemFromCart(state.cartItems, payload)
+      };
+
+    case CartActionTypes.CLEAR_ITEM_FROM_CART:
+      return {
+        ...state,
+        cartItems: state.cartItems.filter(
+          (cartItem) => cartItem.id !== payload.id
+        )
+      };
 
     default:
       return state;

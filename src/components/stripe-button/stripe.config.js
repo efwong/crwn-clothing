caadb19043cb9dcc18b16ddd1ff83b6f8d0b8d06
Edwin <edwinfwong@gmail.com>
2020-05-25 19:46:06 -0700

    Added keys

---
+++ src/components/stripe-button/stripe.config.js
@@ -0,0 +1,1 @@
+export const stripePublishableKey = 'pk_test_M3ujQIOaketWG3O3hiS5fBoD';
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,10 @@
+export const FirebaseConfig = {
+  apiKey: 'AIzaSyC3VccY1O7Wpu-yRJdKxWjuCNJkQFbbYP8',
+  authDomain: 'crwn-service.firebaseapp.com',
+  databaseURL: 'https://crwn-service.firebaseio.com',
+  projectId: 'crwn-service',
+  storageBucket: 'crwn-service.appspot.com',
+  messagingSenderId: '686037421637',
+  appId: '1:686037421637:web:3f3b2e56b74df3caf29d2c',
+  measurementId: 'G-F22NJEWGJ5'
+};

7fa8e30a9c088073a7b5563035fc8277c8a42992
Edwin <edwinfwong@gmail.com>
2020-05-31 17:54:50 -0700

    Add HOC spinner component

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,42 +1,63 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Route } from 'react-router-dom';
-
-import CollectionsOverview from './../../components/collections-overview/collections-overview.component';
 import CollectionPage from '../collection/collection.component';
 import { updateCollections } from './../../redux/shop/shop.actions';
+import WithSpinner from './../../components/with-spinner/with-spinner.component';
 import {
   firestore,
   convertCollectionsSnapshotToMap
 } from './../../firebase/firebase.utils';
 
-class ShopPage extends React.Component {
-  unsubscribeFromSnapshot = null;
+import CollectionsOverview from './../../components/collections-overview/collections-overview.component';
 
-  constructor() {
-    super();
-    this.state = {};
-  }
+const CollectionsOverviewWithSpinner = WithSpinner(CollectionsOverview);
+const CollectionsPageWithSpinner = WithSpinner(CollectionPage);
+
+class ShopPage extends React.Component {
+  state = {
+    loading: true,
+    test: 'abc'
+  };
+
+  unsubscribeFromSnapshot = null;
 
   componentDidMount() {
     const { updateCollections } = this.props;
     const collectionRef = firestore.collection('collections');
     // when collectionRef updates or when this code is ran, firestore will send collection snapshot containing data
-    collectionRef.onSnapshot(async (snapshot) => {
-      const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
-      console.log('collectionsMap', collectionsMap);
-      updateCollections(collectionsMap);
-    });
+    this.unsubscribeFromSnapshot = collectionRef.onSnapshot(
+      async (snapshot) => {
+        const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
+        updateCollections(collectionsMap);
+        this.setState({ loading: false }, () => {
+          console.log('state', this.state);
+        });
+      }
+    );
+  }
+
+  componentWillUnmount() {
+    this.unsubscribeFromSnapshot();
   }
 
   render() {
     const { match } = this.props;
+    const { loading } = this.state;
     return (
       <div className='shop-page'>
-        <Route exact path={`${match.path}`} component={CollectionsOverview} />
+        <Route
+          exact
+          path={`${match.path}`}
+          render={(props) => (
+            <CollectionsOverviewWithSpinner isLoading={loading} {...props} />
+          )}
+        />
         <Route
           path={`${match.path}/:collectionId`}
-          component={CollectionPage}
+          render={(props) => (
+            <CollectionsPageWithSpinner isLoading={loading} {...props} />
+          )}
         />
       </div>
     );

e66f30e9eb23a0e6acfc20d20a25e2e18659fdc4
Edwin <edwinfwong@gmail.com>
2020-05-31 17:54:50 -0700

    Add HOC spinner component

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -2,11 +2,14 @@ import React from 'react';
 
 import './collection-preview.styles.scss';
 import CollectionItem from '../collection-item/collection-item.component';
+import { Link } from 'react-router-dom';
 
 const CollectionPreview = ({ title, items }) => {
   return (
     <div className='collection-preview'>
-      <h1>{title.toUpperCase()}</h1>
+      <h1>
+        <Link to={`/shop/${title.toLowerCase()}`}>{title.toUpperCase()}</Link>
+      </h1>
       <div className='preview'>
         {items
           .filter((item, idx) => idx < 4)

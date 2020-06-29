f278d387b6982d19cc6f00c8b9c5c00929388911
Edwin <edwinfwong@gmail.com>
2020-06-29 00:45:31 -0700

    separating our cra app into client and server, added express server to help handle backend stripe

---
+++ client/build/precache-manifest.8f71cf6dfadff085ce88d7f255069c98.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a10aa588f5334b4d543109e0ba8772ce",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f6ebd9224533c4875135",
+    "url": "/static/css/main.4b0d08d0.chunk.css"
+  },
+  {
+    "revision": "b10bcd14464e84014af3",
+    "url": "/static/js/2.58d05ad5.chunk.js"
+  },
+  {
+    "revision": "f9b3e87900b5f3843f2b97c9b5b24d1e",
+    "url": "/static/js/2.58d05ad5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6ebd9224533c4875135",
+    "url": "/static/js/main.658508c8.chunk.js"
+  },
+  {
+    "revision": "9dce950c1f8d5f2c45b4",
+    "url": "/static/js/runtime-main.a4da908e.js"
+  },
+  {
+    "revision": "19ef7b17bc9414c63c7781251ce0d24d",
+    "url": "/static/media/crown.19ef7b17.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/static/media/shopping-bag.30f07071.svg"
+  }
+]);

83ffa9d89ec721add277fc40175f638c60579652
Edwin <edwinfwong@gmail.com>
2020-10-07 02:14:43 -0700

    Updated gitignore

---
+++ client/build/precache-manifest.3de27e2aa362b69ce854a68f4b52b637.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3149dc7c6b5445a339f14387f627546b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4fb7c1c177a15a6167c5",
+    "url": "/static/css/main.487e8448.chunk.css"
+  },
+  {
+    "revision": "937198bcb2c3edd05be7",
+    "url": "/static/js/2.d542cf1c.chunk.js"
+  },
+  {
+    "revision": "f9b3e87900b5f3843f2b97c9b5b24d1e",
+    "url": "/static/js/2.d542cf1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4fb7c1c177a15a6167c5",
+    "url": "/static/js/main.2e450ead.chunk.js"
+  },
+  {
+    "revision": "9dce950c1f8d5f2c45b4",
+    "url": "/static/js/runtime-main.a4da908e.js"
+  },
+  {
+    "revision": "19ef7b17bc9414c63c7781251ce0d24d",
+    "url": "/static/media/crown.19ef7b17.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/static/media/shopping-bag.30f07071.svg"
+  }
+]);
